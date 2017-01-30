3be7402eeb29903580e5c86f832e071aefb7f3ac
Kevin <k3.22@live.com.mx>
2017-01-29 19:37:05 -0600

    Proyecto terminado

--- js/main.js
+++ js/main.js
@@ -3,4 +3,15 @@ $(document).ready(function(){
       $('.slider').slider();
       $('.parallax').parallax();
       $('.carousel').carousel();
+      $('select').material_select();
 });
+$("#enviar").click(function(){
+  $("confirmacion").fadeOut(3000);
+  $("#mensaje").html("Gracias por hacer su solicitud de compra a la brevedad recibirá en un mensaje en su correo con la confirmación de los datos: ");
+  var nombre= $("#first_name").val();
+  var apellido= $("#last_name").val();
+  var email= $("#email").val();
+  nom= nombre +" " +apellido;
+  $("#nombre").html("Nombre: " + " " +nom);
+  $("#correo").html("Email: "+ " " +email);
+})
